02ddb90c2b1afcaec2d1fe7045fea91ac41c0784
mateusscardosoo <cardosorocha27@hotmail.com>
2024-04-05 18:47:11 -0300

    [FEAT] Convertendo tela de login para Class Component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      message: ''
+    };
+  }
 
-  const handleLogin = (e) => {
+  handleLogin = (e) => {
     e.preventDefault(); 
+    const { email, password } = this.state;
     if (email === 'eduardo.lino@pucpr.br' && password === '123456') {
-      setMessage('Acessado com sucesso!');
+      this.setState({ message: 'Acessado com sucesso!' });
     } else {
-      setMessage('Usuário ou senha incorretos!');
+      this.setState({ message: 'Usuário ou senha incorretos!' });
     }
   };
 
-  return (
-    <div className='App'>
-
-    <form onSubmit={handleLogin}>
-      <h1>Login</h1>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-      </div>
-      <div>
-        <label>Senha:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Acessar</button>
-      <div className='message'>{message}</div>
-    </form>
+  render() {
+    const { email, password, message } = this.state;
+    return (
+      <div className='App'>
+        <form onSubmit={this.handleLogin}>
+          <h1>Login</h1>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
-  );
+          <div>
+            <label>Senha:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </div>
+          <button type="submit">Acessar</button>
+          <div className='message'>{message}</div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
