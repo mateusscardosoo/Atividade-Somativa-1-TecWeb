ed110e44a45960467ccef44ddc920358aa6a39d7
mateusscardosoo <cardosorocha27@hotmail.com>
2024-04-05 18:30:11 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault(); 
+    if (email === 'eduardo.lino@pucpr.br' && password === '123456') {
+      setMessage('Acessado com sucesso!');
+    } else {
+      setMessage('Usuário ou senha incorretos!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+
+    <form onSubmit={handleLogin}>
+      <h1>Login</h1>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
+      </div>
+      <div>
+        <label>Senha:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Acessar</button>
+      <div className='message'>{message}</div>
+    </form>
+          </div>
   );
 }
 
